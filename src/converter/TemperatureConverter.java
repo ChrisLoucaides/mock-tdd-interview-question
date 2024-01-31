64e632a63b057bf753faae56f527943795330189
ChrisLoucaides <christosloucaides@gmail.com>
2024-01-31 11:42:53 +0000

    Stripped TemperatureConverter of all functionality and removed test class around it

--- src/converter/TemperatureConverter.java
+++ src/converter/TemperatureConverter.java
@@ -1,7 +1,4 @@
 package converter;
 
 public class TemperatureConverter {
-    public double convertFromCelsiusToFahrenheit(double temperatureInCelsius) {
-        return temperatureInCelsius * 1.8 + 32;
-    }
 }
